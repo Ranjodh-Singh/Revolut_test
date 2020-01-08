04f743d974336f09de9ed1ba5f2a2377b11aec38
ranjodh-singh <ranjodh_singh@apple.com>
2020-01-08 21:10:51 +0530

    added custom port to run.

--- src/main/java/test/revolut/App.java
+++ src/main/java/test/revolut/App.java
@@ -4,8 +4,10 @@ import com.sun.jersey.api.container.httpserver.HttpServerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.net.httpserver.HttpServer;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import test.revolut.util.BankConstants;
 
 import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
@@ -21,9 +23,22 @@ import java.util.Map;
  */
 public class App {
   static  Logger logger = LoggerFactory.getLogger(App.class);
+    static int port = BankConstants.DEFAULT_PORT;
 
     public static void main( String[] args ) throws IOException {
 
+
+        if(args!=null && args.length==1){
+            try {
+                port =  Integer.parseInt(args[0]);
+            } catch (Exception e) {
+               logger.error("Invalid command line arguments.");
+            }
+        }
+        if(port >65535 || port < 100){
+            logger.error("Invalid command line arguments.Enter between 100 and 65535, defaulting to 8085");
+            port = BankConstants.DEFAULT_PORT;
+        }
         logger.info("Starting Bank's Embedded  HTTPServer...\n");
         HttpServer basicHttpServer = createHttpServer();
         basicHttpServer.start();
@@ -40,7 +55,7 @@ public class App {
         return HttpServerFactory.create(getBaseURI(), applicationResourceConfig);
     }
     private static URI getBaseURI() {
-        return UriBuilder.fromUri("http://" + getHostName() + "/").port(8085).build();
+        return UriBuilder.fromUri("http://" + getHostName() + "/").port(port).build();
     }
 
     private static String getHostName() {
--- src/main/java/test/revolut/util/BankConstants.java
+++ src/main/java/test/revolut/util/BankConstants.java
@@ -3,6 +3,8 @@ package test.revolut.util;
 public class BankConstants {
     public static final String LOG = "BankLog";
     public static final String DEFAULT_CURRENCY = "USD";
+    public static final int DEFAULT_PORT = 8085;
+
     public enum Action{
         CREDIT,DEBIT,TRANSFERRED
     }
